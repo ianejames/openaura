34c6b7f249275cd6d0ee95225e4cdca626547275
Ian James <ianejames@yahoo.com>
2015-04-01 16:30:43 -0700

    Interleave particles

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,5 @@
 require 'openaura'
+require 'core_ext/array/interleave'
 
 class ArtistsController < ApplicationController
   class Artist
@@ -23,6 +24,22 @@ class ArtistsController < ApplicationController
 
     # Given an array of particles, sorts them and returns the sorted array.
     def sort_particles(particles)
-      particles
+      particles_by_source_id = particles.group_by {|p| p['source']['oa_source_id'] }
+
+      # The following code generates an array of hashes, each hash
+      # representing one source. The hash has the id of the source, an array
+      # of particles from that source, and the date of the most recent
+      # particle.
+      sources = particles_by_source_id.map do |source_id, particles|
+        max_date = particles.map {|p| Date.parse(p['date']) }.max
+        { id: source_id, particles: particles, max_date: max_date }
+      end
+
+      # Source with most recent particles sorted first.
+      sources = sources.sort_by {|s| s[:max_date] }.reverse
+
+      # Creates an array of particle groups, then chooses them in a
+      # round-robin fashion to create a flat array of particles.
+      sources.map {|s| s[:particles] }.interleave
     end
 end
