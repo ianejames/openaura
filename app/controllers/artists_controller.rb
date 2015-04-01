63e31ece1a8e55c9282035ec939bdf7646cce347
Ian James <ianejames@yahoo.com>
2015-04-01 16:20:31 -0700

    Create artist class and variable

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,10 @@
 require 'openaura'
 
 class ArtistsController < ApplicationController
+  class Artist
+    attr_accessor :name, :particles
+  end
+
   before_action :set_artist, only: [:show]
 
   def show
@@ -11,5 +15,9 @@ class ArtistsController < ApplicationController
     def set_artist
       @classic_resource = Openaura::ClassicResource.new(params[:id], ENV['OPENAURA_API_KEY'])
       @particles_resource = Openaura::ParticlesResource.new(params[:id], ENV['OPENAURA_API_KEY'])
+
+      @artist = Artist.new
+      @artist.name = @classic_resource.name
+      @artist.particles = @particles_resource.particles
     end
 end
