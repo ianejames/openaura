a553d9c39245fa20e0a0e599a1460e1b47c5b64e
Ian James <ianejames@yahoo.com>
2015-04-01 15:36:30 -0700

    Set artist in ArtistController#show

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,14 @@
+require 'openaura'
+
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show]
+
   def show
   end
+
+  private
+
+    def set_artist
+      @artist = Openaura::ClassicResource.new(params[:id], ENV['OPENAURA_API_KEY'])
+    end
 end
