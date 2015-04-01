f67478f2a76035290f0e7023c901ecccfb3fd4b2
Ian James <ianejames@yahoo.com>
2015-04-01 16:40:25 -0700

    Add 'chunk_size' option to Array#interleave

--- lib/core_ext/array/interleave.rb
+++ lib/core_ext/array/interleave.rb
@@ -1,13 +1,20 @@
 class Array
   # Takes an item from each subarray in order and returns the
   # resulting array. The subarrays do not need to be the same size.
-  def interleave
+  #
+  # The options has takes a 'chunk_size' parameter, which determines
+  # how many items are taken from each subarray on each pass.
+  def interleave(options = {})
+    chunk_size = options.fetch(:chunk_size, 1)
+
     max_length = map(&:size).max
     output = []
     max_length.times do |i|
       each do |subarray|
-        index = i
-        output << subarray[index] if index < subarray.length
+        chunk_size.times do |j|
+          index = i*chunk_size + j
+          output << subarray[index] if index < subarray.length
+        end
       end
     end
     output
